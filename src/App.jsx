508523c16a179adf26d98c6c74781148202f496d
c0dev1king <austinjhiggins@pm.me>
2025-01-09 12:49:54 -0330

    More UI added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import { useState } from 'react'
 import 'leaflet/dist/leaflet.css';
 import Map from './Map.jsx';
-import githubLogo from './assets/github-dark.svg'
+import githubLogo from './assets/github-light.svg'
 import pbwLogo from './assets/project-blue-waters.png'
 import './stylesheet.css'
 
 function App() {
   return (
     <div className="relative">
-      <div className="w-50 h-[2vh] bg-[#15b7f2] text-white font-bold flex items-center justify-center pointer-events-auto">
-        Endangered Species Map
+      <div className="w-50 h-[2vh] bg-white text-black font-bold flex items-center justify-start px-2 pointer-events-auto">
+        Aquatic Endangered Species Map
       </div>
       <Map />
-      <div className='h-[8vh] w-full bg-[#15b7f2] text-white flex items-center justify-center px-2'>
+      <div className='h-[8vh] w-full bg-white text-black flex items-center justify-around px-2'>
         <div className='flex justify-center'>
           Created with ❤️ by <a href='https://github.com/c0dev1king' className='ml-1 hover:text-orange-500'>c0dev1king</a>
           <img src={githubLogo} className='w-6 h-6 ml-2' />
         </div>
+        <div className='flex flex-row justify-evenly'>
+          <ul>
+            <li>Sources</li>
+            <li>Get Involved</li>
+          </ul>
+          <ul>
+            <li>Resources</li>
+          </ul>
+        </div>
       </div>
-      <img src={pbwLogo} className='h-[7vh] absolute bottom-1 right-1' />
+      <img src={pbwLogo} className='h-[7vh] absolute bottom-1 right-1 shadow-[2px_2px_2px_rgba(0,0,0,0.2)]' />
     </div>
   )
 }
